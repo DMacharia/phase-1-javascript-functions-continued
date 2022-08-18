253bb8deaacce05b7f0619a974e12a7232d92ba4
Daniel Macharia <danielhuios@gmail.com>
2022-08-18 06:45:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(funActivity = "roller-skate") {
+  return `This Saturday, I want to ${funActivity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+const mondayWork = function (task = "go to the office") {
+  return `This Monday, I will ${task}.`;
+};
+mondayWork();
+
+function wrapAdjective(emphasis = "*") {
+  return function (part1 = "special") {
+    return `You are ${emphasis}${part1}${emphasis}!`;
+  };
+}
+wrapAdjective()("a hard worker");
+wrapAdjective("||")("a dedicated programmer");
